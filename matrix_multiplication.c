92614f6683a4384494c11187ed9b33827c6624e8
Harsha G <harsha150904@gmail.com>
2026-05-24 21:48:29 +0530

    Implement matrix multiplication in C

---
+++ matrix_multiplication.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[2][2],b[2][2],result[2][2];
+    int i,j,k;
+
+    printf("Enter first matrix:\n");
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("Enter second matrix:\n");
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            result[i][j]=0;
+
+            for(k=0;k<2;k++)
+            {
+                result[i][j]+=a[i][k]*b[k][j];
+            }
+        }
+    }
+
+    printf("\nResult Matrix:\n");
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d ",result[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
